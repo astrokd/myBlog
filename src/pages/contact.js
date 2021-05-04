c952cdfc178020ae6d7edd4e27e0151b2cd88c65
AstroKD <kdreyerinseattle@gmail.com>
2021-05-03 18:26:28 -0700

    dropdown with css, doesn't work well

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
         <Layout activelink="Contact">
 
-          Welcome to my Contact Page, here you will find information about how to contact me.
+          <h3>Welcome to my Contact Page, here you will find information about how to contact me.</h3>
         
         </Layout>
   )
