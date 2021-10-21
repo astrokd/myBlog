bf85f0f4ba0dfbb1f0b0e49e32e85b8551b72ec9
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-10-21 12:27:26 -0700

    added more columns and combined tree names

--- src/pages/sdotstreettrees.js
+++ src/pages/sdotstreettrees.js
@@ -10,20 +10,27 @@ const StreetTrees = () => {
     return (
       <>
         <Layout activelink="StreetTrees">
+            <h2>Seattle Department of Transportation – Approved Street Tree List</h2>
                 <Table striped bordered hover size="sm">
                     <thead>
                         <tr>
                             <th>Tree Type</th>
-                            <th>Scientific Name</th>
-                            <th>Common Name</th>
+                            <th>Scientific & Common Name</th>
+                            <th>Mature<br /> Height</th>
+                            <th>Spread</th>
+                            <th>Under<br />Wires?</th>
+                            <th>Min Strip<br />Width</th>
                         </tr>
                     </thead>
                     <tbody>
                         {JSONData.map(data => (
                             <tr key={data.ScientificName}>
                                 <td>{data.TreeType}</td>
-                                <td>{data.ScientificName}</td>
-                                <td>{data.CommonName}</td>
+                                <td>{data.ScientificName} <br />{data.CommonName}</td>
+                                <td>{data.MatureHeight}</td>
+                                <td>{data.Spread}</td>
+                                <td>{data.UnderWires}</td>
+                                <td>{data.MinStripWidth}</td>
                             </tr>
                         ))}
                     </tbody>
