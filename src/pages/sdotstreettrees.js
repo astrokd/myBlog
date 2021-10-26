e6638f2aaef8951a78264515cc608812759d5cfd
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-10-26 13:06:24 -0700

    put StreetTrees in class and added constructor with JSON data as state

--- src/pages/sdotstreettrees.js
+++ src/pages/sdotstreettrees.js
@@ -19,45 +19,89 @@ const Comments = (props) => {
     )
 }
 
+class StreetTrees extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listData: JSONData,
+        }
+    }
 
-// markup
-const StreetTrees = () => {
-    console.log("in streettrees")
-    return (
-      <>
-        <Layout activelink="StreetTrees">
-            <h2>Seattle Department of Transportation – Approved Street Tree List</h2>
-                <Table responsive striped bordered size="sm">
-                    <thead>
-                        <tr>
-                            <th className={treeName}>Scientific & Common Name</th>
-                            <th>Mature<br /> Height</th>
-                            <th>Spread</th>
-                            <th>Under<br />Wires?</th>
-                            <th>Min Strip<br />Width</th>
-                            <th>Flower<br />Color</th>
-                            <th>Fall<br />Color</th>
-                            <th>Comments</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {JSONData.map(data => (
-                            <tr key={data.ScientificName}>
-                                <td className={treeName}>{data.ScientificName} <br />{data.CommonName}</td>
-                                <td>{data.MatureHeight}</td>
-                                <td>{data.Spread}</td>
-                                <td style={(data.UnderWires === "No") ? {backgroundColor:'#FF0000'} : {backgroundColor:'#00FF00'} }>{data.UnderWires}</td>
-                                <td>{data.MinStripWidth}</td>
-                                <td>{(data.FlowerColor === "N/A") ? data.FlowerColor : <Flower className={svgStyle} fill={data.FlowerColor} /> }</td>
-                                <td>{(data.FallColor === "N/A") ? data.FallColor : <Leaf className={svgStyle} fill={data.FallColor} /> }</td>
-                                <td className={treeComment}><Comments Comments={data.Comments} Warnings={data.Warnings || false} GPPLink={data.GPPLink || false}/></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-        </Layout>
-      </>
-    )
-  }
+    render() {
+        return (
+            <>
+              <Layout activelink="StreetTrees">
+                  <h2>Seattle Department of Transportation – Approved Street Tree List</h2>
+                      <Table responsive striped bordered size="sm">
+                          <thead>
+                              <tr>
+                                  <th className={treeName}>Scientific & Common Name</th>
+                                  <th>Mature<br /> Height</th>
+                                  <th>Spread</th>
+                                  <th>Under<br />Wires?</th>
+                                  <th>Min Strip<br />Width</th>
+                                  <th>Flower<br />Color</th>
+                                  <th>Fall<br />Color</th>
+                                  <th>Comments</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              {this.state.listData.map(data => (
+                                  <tr key={data.ScientificName}>
+                                      <td className={treeName}>{data.ScientificName} <br />{data.CommonName}</td>
+                                      <td>{data.MatureHeight}</td>
+                                      <td>{data.Spread}</td>
+                                      <td style={(data.UnderWires === "No") ? {backgroundColor:'#FF0000'} : {backgroundColor:'#00FF00'} }>{data.UnderWires}</td>
+                                      <td>{data.MinStripWidth}</td>
+                                      <td>{(data.FlowerColor === "N/A") ? data.FlowerColor : <Flower className={svgStyle} fill={data.FlowerColor} /> }</td>
+                                      <td>{(data.FallColor === "N/A") ? data.FallColor : <Leaf className={svgStyle} fill={data.FallColor} /> }</td>
+                                      <td className={treeComment}><Comments Comments={data.Comments} Warnings={data.Warnings || false} GPPLink={data.GPPLink || false}/></td>
+                                  </tr>
+                              ))}
+                          </tbody>
+                      </Table>
+              </Layout>
+            </>
+          )
+    }
+}
+
+// const StreetTrees = () => {
+//     return (
+//       <>
+//         <Layout activelink="StreetTrees">
+//             <h2>Seattle Department of Transportation – Approved Street Tree List</h2>
+//                 <Table responsive striped bordered size="sm">
+//                     <thead>
+//                         <tr>
+//                             <th className={treeName}>Scientific & Common Name</th>
+//                             <th>Mature<br /> Height</th>
+//                             <th>Spread</th>
+//                             <th>Under<br />Wires?</th>
+//                             <th>Min Strip<br />Width</th>
+//                             <th>Flower<br />Color</th>
+//                             <th>Fall<br />Color</th>
+//                             <th>Comments</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         {JSONData.map(data => (
+//                             <tr key={data.ScientificName}>
+//                                 <td className={treeName}>{data.ScientificName} <br />{data.CommonName}</td>
+//                                 <td>{data.MatureHeight}</td>
+//                                 <td>{data.Spread}</td>
+//                                 <td style={(data.UnderWires === "No") ? {backgroundColor:'#FF0000'} : {backgroundColor:'#00FF00'} }>{data.UnderWires}</td>
+//                                 <td>{data.MinStripWidth}</td>
+//                                 <td>{(data.FlowerColor === "N/A") ? data.FlowerColor : <Flower className={svgStyle} fill={data.FlowerColor} /> }</td>
+//                                 <td>{(data.FallColor === "N/A") ? data.FallColor : <Leaf className={svgStyle} fill={data.FallColor} /> }</td>
+//                                 <td className={treeComment}><Comments Comments={data.Comments} Warnings={data.Warnings || false} GPPLink={data.GPPLink || false}/></td>
+//                             </tr>
+//                         ))}
+//                     </tbody>
+//                 </Table>
+//         </Layout>
+//       </>
+//     )
+//   }
   
 export default StreetTrees
