c0a4472cd4c1580d0cde9b0bab34e2b83e5f28da
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-10-29 12:52:01 -0700

    added If component and created a tree type row

--- src/pages/sdotstreettrees.js
+++ src/pages/sdotstreettrees.js
@@ -3,16 +3,16 @@ import Table from 'react-bootstrap/Table'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import JSONData from "../data/sdotstreettrees.json"
 import Layout from '../components/layout'
+import If from '../components/if'
 
 import { treeType, treeName, treeComment, svgStyle } from './sdotst.module.css'
 
 import Leaf from '../images/leaf.svg';
 import Flower from '../images/flower.svg';
 
-function TableHeader(props) {
+function TableHeader() {
     return (
         <tr>
-            <th className={treeType}>Tree Type</th>
             <th className={treeName}>Scientific & Common Name</th>
             <th>Mature<br /> Height</th>
             <th>Spread</th>
@@ -28,17 +28,16 @@ function TableHeader(props) {
 function TreeTypeRow({ type }) {
     return (
         <tr>
-            <th colSpan="8">
+            <td className={treeType} colSpan="8">
                 {type}
-            </th>
+            </td>
         </tr>
     );
 }
 
-function TreeRow({ tree, currentType }) {
+function TreeRow({ tree }) {
     return (
         <tr key={tree.ScientificName}>
-            <td className={treeType}>{(tree.TreeType !== currentType) ? (currentType = tree.TreeType) : null}</td>
             <td className={treeName}>{tree.ScientificName} <br />{tree.CommonName}</td>
             <td>{tree.MatureHeight}</td>
             <td>{tree.Spread}</td>
@@ -51,8 +50,6 @@ function TreeRow({ tree, currentType }) {
     );
 }
 
-
-
 const Comments = (props) => {
     return (
         <div style={(props.Warnings) ? {backgroundColor:'yellow'} : {backgroundColor:'transparent'} } >
@@ -62,7 +59,6 @@ const Comments = (props) => {
         </div>
     )
 }
-
 
 class StreetTrees extends React.Component {
     constructor(props) {
@@ -84,7 +80,12 @@ class StreetTrees extends React.Component {
                           </thead>
                           <tbody>
                               {this.state.listData.map(data => (
-                                  <TreeRow tree={data} currentType={this.state.currentType}/>
+                                  <>
+                                    <If condition={data.TreeType !== this.state.currentType}>
+                                        <TreeTypeRow type={this.state.currentType = data.TreeType}/>
+                                    </If>
+                                    <TreeRow tree={data} />
+                                  </>
                               ))}
                           </tbody>
                       </Table>
@@ -93,43 +94,5 @@ class StreetTrees extends React.Component {
           )
     }
 }
-
-// const StreetTrees = () => {
-//     return (
-//       <>
-//         <Layout activelink="StreetTrees">
-//             <h2>Seattle Department of Transportation – Approved Street Tree List</h2>
-//                 <Table responsive striped bordered size="sm">
-//                     <thead>
-//                         <tr>
-//                             <th className={treeName}>Scientific & Common Name</th>
-//                             <th>Mature<br /> Height</th>
-//                             <th>Spread</th>
-//                             <th>Under<br />Wires?</th>
-//                             <th>Min Strip<br />Width</th>
-//                             <th>Flower<br />Color</th>
-//                             <th>Fall<br />Color</th>
-//                             <th>Comments</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {JSONData.map(data => (
-//                             <tr key={data.ScientificName}>
-//                                 <td className={treeName}>{data.ScientificName} <br />{data.CommonName}</td>
-//                                 <td>{data.MatureHeight}</td>
-//                                 <td>{data.Spread}</td>
-//                                 <td style={(data.UnderWires === "No") ? {backgroundColor:'#FF0000'} : {backgroundColor:'#00FF00'} }>{data.UnderWires}</td>
-//                                 <td>{data.MinStripWidth}</td>
-//                                 <td>{(data.FlowerColor === "N/A") ? data.FlowerColor : <Flower className={svgStyle} fill={data.FlowerColor} /> }</td>
-//                                 <td>{(data.FallColor === "N/A") ? data.FallColor : <Leaf className={svgStyle} fill={data.FallColor} /> }</td>
-//                                 <td className={treeComment}><Comments Comments={data.Comments} Warnings={data.Warnings || false} GPPLink={data.GPPLink || false}/></td>
-//                             </tr>
-//                         ))}
-//                     </tbody>
-//                 </Table>
-//         </Layout>
-//       </>
-//     )
-//   }
   
 export default StreetTrees
