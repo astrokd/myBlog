3f4f9828acfacc48dedadb7a2ed25b0a10380789
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-11-17 11:35:18 -0800

    Created static query, placed each project in a cta component, listed tools used

--- src/components/projlist.js
+++ src/components/projlist.js
@@ -1,47 +1,52 @@
 import * as React from "react"
 import { Link } from 'gatsby'
-import { graphql } from "gatsby"
-
-export const query = graphql`
-  query {
-    allMarkdownRemark(filter: {frontmatter: {type: {eq: "proj"}}}) {
-      nodes {
-        html
-        id
-        frontmatter {
-            date
-            repo
-            slug
-            title
-            tools
-            url
-        }
-      }
-    }
-  }
-`;
+import CallToAction from '../components/callToAction'
+import { StaticQuery, graphql } from "gatsby"
 
 // markup
 const ProjList = ({ data }) => {
-    console.log("data: " + data)
-    const projects = data.allMarkdownRemark.nodes
-  return (
-    <>
-        <div>
-            <br></br>
-            <h3>Here is a list of projects</h3>
-            <ul>
-                {projects.map(node => (
-                    <li key={node.id}>
-                        <div>{node.frontmatter.title} - created on: {node.frontmatter.date}</div>
-                        <Link to={node.frontmatter.url}>Go to url</Link>
-                        <div>{(node.frontmatter.url)}</div>
-                    </li>
-                ))}
-            </ul>
-        </div>              
-    </>
-  )
+    return (
+        <StaticQuery
+            query={graphql`
+            query Projects {
+                allMarkdownRemark(filter: {frontmatter: {type: {eq: "proj"}}}) {
+                    nodes {
+                        html
+                        id
+                        frontmatter {
+                            date
+                            repo
+                            slug
+                            title
+                            tools
+                            url
+                        }
+                    }
+                }
+            }
+        `}
+      render={data => (
+        <>
+            <div>
+                <br></br>
+                <h3>Here is a list of projects</h3>
+                <ul>
+                    {data.allMarkdownRemark.nodes.map(node => (
+                        <li key={node.id}>
+                            <CallToAction>
+                                <div>{node.frontmatter.title} - created on: {node.frontmatter.date}</div>
+                                <Link to={node.frontmatter.url}>Go to url</Link>
+                                <div>Tools used: {node.frontmatter.tools.toString()}</div>
+                            </CallToAction>
+                        </li>
+
+                    ))}
+                </ul>
+            </div>              
+        </>
+      )}
+    />
+    )
 }
 
 export default ProjList
