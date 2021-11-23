206c7daeefdd0b4f1c3ba05a015bb3aef72faac8
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-11-23 11:09:58 -0800

    fixed proj path

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,11 +7,11 @@ import { topnav, navlink, active } from './navbar.module.css'
 const NavBar = (props) => {
     return (
       <div className={topnav}>
-        <div className={(props.activelink === "Home") ? active : navlink}><Link to="/">Home</Link></div>
+        <div className={(props.activelink === "Home") ? active : navlink}><Link to="/">Homepage</Link></div>
         <div className={(props.activelink === "About") ? active : navlink}><Link to="/about">About</Link></div>
         <div className={(props.activelink === "Contact") ? active : navlink}><Link to="/contact">Contact</Link></div>
         <div className={(props.activelink === "Blog") ? active : navlink}><Link to="/blog">Blog</Link></div>
-        <div className={(props.activelink === "Proj") ? active : navlink}><Link to="/blog">Projects</Link></div>
+        <div className={(props.activelink === "Proj") ? active : navlink}><Link to="/proj">Projects</Link></div>
       </div>
       )
 }
